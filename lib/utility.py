fa7c43014e9bd180f2830f37b23d5680b45ec28e
gavanderlinden <tony.van_der_linden@holidaycheck.com>
2017-05-12 13:54:04 +0200

    added interactive exercises inline
    - added a class QA to utility allowing to ask simple questions and execute the correct answer afterwards
    - added some exercises to basics 2-1

--- lib/utility.py
+++ lib/utility.py
@@ -1,4 +1,8 @@
+import json
+from pathlib import Path
 from functools import wraps
+
+__dir__ = Path(__file__).absolute().parent
 
 
 def check_output(expected=None):
@@ -44,4 +48,41 @@ def check_answer(_globals, answers):
         print("CORRECT")
 
 
-           
+class QA(object):
+
+    def __init__(self, file_name):
+        self.__file_path = __dir__.parent/"data"/("%s.json" % file_name)
+        self.__data = json.load(self.__file_path.open())
+
+    def run(self, _id):
+        for question in self.__data[_id]:
+            yield self.ask_question(question)
+        print("GOOD JOB!")
+
+    def ask_question(self, data):
+        question = data["question"]
+        answer = data["answer"]
+        method = data["method"]
+        while True:
+
+            # just execute some code
+            if method == "declare":
+                print(question)
+                return answer
+
+            code = self.standardize_code(input(question + ": "))
+            if method == "text":
+                result = (answer == code)
+                code = ""
+            else:
+                result = (code == answer)
+
+            if result:
+                print("CORRECT")
+                return code
+            else:
+                print("WRONG")
+
+    @staticmethod
+    def standardize_code(code):
+        return code.replace('"', "'")
